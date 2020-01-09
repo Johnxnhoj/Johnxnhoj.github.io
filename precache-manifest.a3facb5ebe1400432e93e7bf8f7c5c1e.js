750eb3609092dd1a2ba9807fa8a4785e6a5256b8
John Carbajal <johnxjuanito@gmail.com>
2020-01-08 17:05:13 -0800

    johns ak

---
+++ precache-manifest.a3facb5ebe1400432e93e7bf8f7c5c1e.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de212879e882c3c87a7ff4dcf3f974ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6648b433021fd078f90",
+    "url": "/static/css/2.400f043a.chunk.css"
+  },
+  {
+    "revision": "0fdb734fb0d3a8db0acf",
+    "url": "/static/css/main.8cd68a40.chunk.css"
+  },
+  {
+    "revision": "a6648b433021fd078f90",
+    "url": "/static/js/2.bad731c5.chunk.js"
+  },
+  {
+    "revision": "0fdb734fb0d3a8db0acf",
+    "url": "/static/js/main.645769a6.chunk.js"
+  },
+  {
+    "revision": "767e35795de6c22d9fb1",
+    "url": "/static/js/runtime-main.8bf9c574.js"
+  },
+  {
+    "revision": "b68e36877dfcd48d5d66f6def1f5949c",
+    "url": "/static/media/4k picture of vegas.b68e3687.jpg"
+  },
+  {
+    "revision": "add3b857f10d3c2ebdcbd94a7af4526f",
+    "url": "/static/media/IMG_0174.add3b857.jpg"
+  },
+  {
+    "revision": "47ce6e77f039020ee2e76a10c1e988e9",
+    "url": "/static/media/React.js_logo-512.47ce6e77.png"
+  },
+  {
+    "revision": "af6d6985c8abd86d9f332f2a8ac1b9d4",
+    "url": "/static/media/Screen Shot 2020-01-02 at 4.31.07 PM.af6d6985.png"
+  },
+  {
+    "revision": "02d4c0a4f9c3d888294495a6a71d54c0",
+    "url": "/static/media/Screen Shot 2020-01-04 at 2.41.42 PM.02d4c0a4.png"
+  },
+  {
+    "revision": "7aade4333ff496b4912c29f6804df419",
+    "url": "/static/media/Screen Shot 2020-01-04 at 4.13.26 PM.7aade433.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "f80a91ba03fbf0e99ea6949aa3a6399e",
+    "url": "/static/media/mysql-logo.f80a91ba.svg"
+  },
+  {
+    "revision": "46f758661588703b190fb4d2aae3847a",
+    "url": "/static/media/node-logo.46f75866.png"
+  },
+  {
+    "revision": "446763959684b108ca164f3cdc9c96d0",
+    "url": "/static/media/ps-logo.44676395.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.38efc2dbea867fa9033349e7ca433ef3.js"
+  "/precache-manifest.a3facb5ebe1400432e93e7bf8f7c5c1e.js"
 );
 
 self.addEventListener('message', (event) => {
